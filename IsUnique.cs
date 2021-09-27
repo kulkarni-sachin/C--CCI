8536c6d2346dad31de449569d8e98d02499f5574
Sachin Kulkarni <itssachin.kulkarni@gmail.com>
2021-09-27 17:56:58 +0530

    Update IsUnique.cs
    updated the indentation of the code

--- IsUnique.cs
+++ IsUnique.cs
@@ -5,21 +5,21 @@ public class Program
 {
 	public static void Main()
 	{
-		 string input = "SAA";
-        bool isUnique = IsUnique(input);
+		string input = "SAA";
+        	bool isUnique = IsUnique(input);
 		Console.WriteLine(isUnique);
 	}
 	
 	public static bool IsUnique(string input)
-    {
-        bool[] array = new bool[128];
-        foreach (char b in input.ToCharArray())
-        {
-			if(array[b])
-            	return false;
-			else
-				array[b] = true;
-        }
-		return true;
-    }
+    	{
+		bool[] array = new bool[128];
+		foreach (char b in input.ToCharArray())
+		{
+				if(array[b])
+			return false;
+				else
+					array[b] = true;
+		}
+			return true;
+    	}
 }
